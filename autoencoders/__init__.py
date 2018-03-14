80255718da0c361e6ff72725060df915f4e3a47b
Mitiku <se.mitiku.yohannes@gmail.com>
2018-03-14 13:17:07 +0300

    added cmd argumentsc

--- autoencoders/__init__.py
+++ autoencoders/__init__.py
@@ -1,5 +1,6 @@
 from keras.layers import Conv2D,MaxPool2D,Dense,UpSampling2D,Input
-from keras.models import Model
+from keras.models import Model,model_from_json
+import json
 import keras
 import os 
 import cv2 
@@ -26,8 +27,18 @@ def auto_encoder_model(input_shape):
     model = Model(inputs=input_layer,outputs=decoded)
     model.summary()
     model.compile(loss=keras.losses.mean_absolute_error,optimizer=keras.optimizers.Adam(lr=1e-4))
-
+    # model_json = model.to_json()
+    # with open("model.json","w+") as json_file:
+    #     json_file.write(model_json)
+    
+    # exit(0)
     return model
+def load_model(json_file,weights):
+    with open(json_file) as j_file:
+        model_json = j_file.read()
+        model = model_from_json(model_json)
+        model.load_weights(weights)
+        return model
 
 def load_images(images_path,output_shape):
     imgs_files = os.listdir(images_path)
@@ -45,7 +56,69 @@ def load_images(images_path,output_shape):
     output = output.astype(np.float32)/255
     train,test = train_test_split(output,test_size=0.2)
     return train,test
+def load_aflw_dataset(dataset_path,inputs_shape):
+    dataset = load_aflw_face_dataset_image_paths(dataset_path)
+    train,test = train_test_split(dataset,test_size=0.01)
+    print(len(test))
+    test_images = load_images_from_paths(dataset_path, test,inputs_shape)
 
-def train(model,dataset):
-    model.fit(dataset[0],dataset[0],validation_data=(dataset[1],dataset[1]),epochs=20,batch_size=32)
-    model.save_weights("model.h5")
+    return train,test_images
+
+
+def load_aflw_face_dataset_image_paths(dataset_dir):
+    output = []
+    for img_file in os.listdir(dataset_dir):
+        if not img_file.endswith(".jpg"):
+            print img_file
+        output.append(img_file)
+    return output
+def load_images_from_paths(dataset_dir,imgs_files,output_shape):
+    output = np.zeros((len(imgs_files),output_shape[0],output_shape[1],output_shape[2]))
+    for i in range(len(imgs_files)):
+        try:
+            image = cv2.imread(os.path.join(dataset_dir,imgs_files[i]))
+        except:
+            print "Unable to read from : ",os.path.join(dataset_dir,imgs_files[i])
+        if image is None:
+            continue
+        try:
+            image = cv2.resize(image,(output_shape[0],output_shape[1]))
+        except:
+            continue
+        try:
+            if output_shape[2]==1:
+                image = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+                image = image.reshape(output_shape[0],output_shape[1],output_shape[2])
+        except:
+            continue
+        output[i] = image
+    return output
+
+def vae_loss(y_true, y_pred):
+    """ Calculate loss = reconstruction loss + KL loss for each data in minibatch """
+    # E[log P(X|z)]
+    recon = K.sum(K.binary_crossentropy(y_pred, y_true), axis=1)
+    # D_KL(Q(z|X) || P(z|X)); calculate in closed form as both dist. are Gaussian
+    kl = 0.5 * K.sum(K.exp(log_sigma) + K.square(mu) - 1. - log_sigma, axis=1)
+
+    return recon + kl
+
+
+def generator(dataset_dir,image_files,batch_size,input_shape):
+    image_files = np.array(image_files)
+    while True:
+        indexes = range(len(image_files))
+        np.random.shuffle(indexes)
+        for i in range(0,len(indexes)-batch_size-1,batch_size):
+            current_indexes = indexes[i:i+batch_size]
+            current_files = image_files[current_indexes]
+            current_images = load_images_from_paths(dataset_dir,current_files,input_shape)
+            current_images = current_images.astype(np.float32)/255
+            yield current_images,current_images
+            
+
+def train(model,dataset_dir,train_img_files,test_images,input_shape,batch_size=32,epochs=10,steps=1000):
+    # model.fit(dataset[0],dataset[0],validation_data=(dataset[1],dataset[1]),epochs=20,batch_size=32)
+    model.fit_generator(generator(dataset_dir,train_img_files,batch_size,input_shape),steps_per_epoch=steps,epochs=epochs,validation_data=(test_images,test_images))
+
+    model.save_weights("model2.h5")
--- autoencoders/__main__.py
+++ autoencoders/__main__.py
@@ -1,19 +1,44 @@
-from  autoencoders import train,load_images,auto_encoder_model
+from  autoencoders import train,load_aflw_dataset,auto_encoder_model,load_model
 import numpy as np
 import os
 import cv2
 import scipy
+import keras
+import argparse
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d","--dataset_path",default="/home/mtk/datasets/img_align_celeba/img_align_celeba",type=str)
+    parser.add_argument('-i', '--input_shape', nargs=3,default=[48,48,3], action='append')
+    parser.add_argument("-b","--batch_size",default=32,type=int)
+    parser.add_argument("-e","--epochs",default=10,type=int)
+    parser.add_argument("-s","--steps",default=1000,type=int)
+    parser.add_argument("-l","--lr",default=1e-4,type=float)
+    return   parser.parse_args()
 
 
 def main():
-    input_shape = (48,48,1)
-    model = auto_encoder_model(input_shape)
-    dataset = load_images("/home/mtk/dataset/aflw/aflw/face",(input_shape))
-    dataset = dataset
-    train(model,dataset)
+    
+    args = get_args()
+    input_shape = tuple(args.input_shape)
+    # model = auto_encoder_model(input_shape)
+    dataset_path =args.dataset_path
+    batch_size = args.batch_size
+    epochs = args.epochs
+    steps = args.steps
+    lr = steps.lr
+
+
+    model = load_model("model.json","model.h5")
+    model.compile(loss=keras.losses.binary_crossentropy,optimizer=keras.optimizers.Adam(lr=lr))
+    # dataset = load_images("/home/mtk/dataset/aflw/aflw/face",(input_shape))
+    training,test = load_aflw_dataset(dataset_path,(input_shape))
+    # training,test = load_aflw_dataset("/home/mtk/datasets/img_align_celeba/img_align_celeba",(input_shape))
+    test = test.astype(np.float32)/255
+    train(model,dataset_path,training,test, input_shape,batch_size=batch_size,epochs=epochs,steps=steps)
 if __name__ == "__main__":
     main()
-    # input_shape = (48,48,1)
+    # input_shape = (48,48,3)
     # image_files = os.listdir("/home/mtk/dataset/aflw/aflw/face")
     # np.random.shuffle(image_files)
     # model = auto_encoder_model(input_shape)
